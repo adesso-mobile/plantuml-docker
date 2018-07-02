bd70a2b1640a77bf7d4dbd05c252962e4ce7a23d
Dirk Heilig <heilig@adesso-mobile.de>
2018-07-02 11:52:44 +0200

    fix images path

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ set -x
 mkdir /app/build -p
 cd /app
 java -jar /app/lib/plantuml.jar /app/src/*.puml
-find -name *.png -exec mv "{}" /app/build/ \;
+find src -name "*.png" -exec mv "{}" /app/build/ \;
 rm -rf  -- "/app/?"
