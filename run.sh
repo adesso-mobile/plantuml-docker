258d27f10e18f277084f9f78bc6a5caa4cf12fea
Dirk Heilig <heilig@adesso-mobile.de>
2018-07-25 15:01:21 +0200

    set charset to utf8

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -e
 set -x
 mkdir /app/build -p
 cd /app
-java -jar /app/lib/plantuml.jar /app/src/*.puml
+java -jar /app/lib/plantuml.jar  -charset UTF-8 /app/src/*.puml
 find src -name "*.png" -exec mv "{}" /app/build/ \;
 rm -rf  -- "/app/?"
